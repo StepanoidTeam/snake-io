c9c7be2c64316af9215828ef9e889c9df367b1f2
bobanko <mishko.vladimir@gmail.com>
2019-05-04 02:53:04 +0300

    server thoughts/todos

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
 
+//todo (@nik): add your buffer implementation here
+//to keep only top 10 results for hi-scores
+//sort by score
+//keep only 1 user with unique name
 var users = [];
 
+//todo: log requested url/method/body
 app.use((req, res, next) => {
   // console.log(JSON.stringify(req));
   next();
@@ -17,6 +22,8 @@ app.get("/getScore", function(req, res) {
 
 app.post("/setScore", function(req, res) {
   console.log(req.body);
+  //todo: check validity of user
+  //do not push random shit
   let user = req.body;
 
   users.push(user);
@@ -24,7 +31,10 @@ app.post("/setScore", function(req, res) {
   res.json(users);
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-  //
+//todo: add & handle websocket connections (use ws lib?)
+
+const port = 3000; //todo: get from config?
+app.listen(port, function() {
+  //todo: add url to open site in browser from console
+  console.log(`Snake.io listening on port ${port}!`);
 });
