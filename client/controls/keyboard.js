b2c72478c899c3e04dfa14e4fddab3f75749786f
bobanko <mishko.vladimir@gmail.com>
2019-05-05 01:19:31 +0300

    refac(client): keyboard codes used

--- client/controls/keyboard.js
+++ client/controls/keyboard.js
@@ -1,29 +1,33 @@
 let nextMoveX = 1; // ед.измерения след.шага.Если меняю на большие числа,то змея дробится
 let nextMoveY = 0; // ед.измерения след.шага.Если меняю на большие числа,то змея дробится
 
-function checkKey(e) {
-  e = e || window.event;
-  if (e.keyCode == "38") {
-    //up arrow
-    nextMoveX = 0;
-    nextMoveY = -1;
-  } else if (e.keyCode == "40") {
-    //down arrow
-    nextMoveX = 0;
-    nextMoveY = 1;
-  } else if (e.keyCode == "37") {
-    //left arrow
-    nextMoveX = -1;
-    nextMoveY = 0;
-  } else if (e.keyCode == "39") {
-    //right arrow
-    nextMoveX = 1;
-    nextMoveY = 0;
+function checkKey({ code }) {
+  switch (code) {
+    case "ArrowUp": {
+      nextMoveX = 0;
+      nextMoveY = -1;
+      break;
+    }
+    case "ArrowDown": {
+      nextMoveX = 0;
+      nextMoveY = 1;
+      break;
+    }
+    case "ArrowLeft": {
+      nextMoveX = -1;
+      nextMoveY = 0;
+      break;
+    }
+    case "ArrowRight": {
+      nextMoveX = 1;
+      nextMoveY = 0;
+      break;
+    }
   }
 }
 
 export function initKeyboard() {
-  document.body.addEventListener("keydown", checkKey, false);
+  document.body.addEventListener("keydown", checkKey);
 
   return () => ({ x: nextMoveX, y: nextMoveY });
 }
