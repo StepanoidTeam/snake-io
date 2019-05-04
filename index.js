9f33a6ef95f9969fb269998fa069228d810929f1
bobanko <mishko.vladimir@gmail.com>
2019-05-04 19:30:34 +0300

    server websocket basic impl

--- index.js
+++ index.js
@@ -34,7 +34,27 @@ app.post("/setScore", function(req, res) {
 //todo: add & handle websocket connections (use ws lib?)
 
 const port = 3000; //todo: get from config?
+const wsPort = 8080;
 app.listen(port, function() {
   //todo: add url to open site in browser from console
   console.log(`Snake.io listening on port ${port}!`);
+});
+
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: wsPort });
+console.log(`websocket listening on port ${wsPort}`);
+// Broadcast to all.
+wss.on("connection", function connection(ws) {
+  console.log("connected");
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    ws.send("yes it is111");
+
+    setTimeout(function timeout() {
+      ws.send(Date.now());
+    }, 500);
+  });
+
+  ws.send("something");
 });
