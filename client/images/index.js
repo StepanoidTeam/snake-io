e02204e78ee2c5e1c309b3d5d4f1bf8c43e95edf
bobanko <mishko.vladimir@gmail.com>
2019-05-05 22:56:16 +0300

    image lib extracted

---
+++ client/images/index.js
@@ -0,0 +1,45 @@
+import { getImage } from "../helpers/get-image.js";
+import { recolorImage } from "../helpers/recolor-image.js";
+import { getMainColor } from "../helpers/get-main-color.js";
+
+export const IMAGES = {
+  DEBUG: "debug.png",
+  APPLE_RED: "red-apple_1f34e.png",
+  SNAKE_BODY: "full-moon-symbol_1f315.png",
+  SNAKE_JOINT: "new-moon-symbol_1f311.png",
+  SNAKE_HEAD: "pig-face_1f437.png",
+  //SNAKE_HEAD: "smiling-face-with-horns_1f608.png",
+  SNAKE_HEAD: "frog-face_1f438.png"
+};
+
+export const IMAGE_LIB = (async () => {
+  const _imageLib = new Map();
+  await Promise.all(
+    Object.entries(IMAGES).map(async ([key, value]) => {
+      _imageLib.set(value, await getImage(value));
+    })
+  );
+
+  //custom shit
+  const recolorOpacity = (200).toString(16).padStart(2, "0");
+
+  //todo: what if i want to add some after init?
+  //ie for snake body recoloring depending on player selection, etc?
+  _imageLib.set(
+    IMAGES.SNAKE_BODY,
+    recolorImage(
+      _imageLib.get(IMAGES.SNAKE_BODY),
+      getMainColor(_imageLib.get(IMAGES.SNAKE_HEAD)) + recolorOpacity
+    )
+  );
+
+  _imageLib.set(
+    IMAGES.SNAKE_JOINT,
+    recolorImage(
+      _imageLib.get(IMAGES.SNAKE_JOINT),
+      getMainColor(_imageLib.get(IMAGES.SNAKE_HEAD)) + recolorOpacity
+    )
+  );
+
+  return _imageLib;
+})();
