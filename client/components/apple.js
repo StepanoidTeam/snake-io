ea2b1a90c897c30d518bb819e23711f6dde99e71
bobanko <mishko.vladimir@gmail.com>
2019-05-10 02:09:37 +0300

    client: much random bonuses

--- client/components/apple.js
+++ client/components/apple.js
@@ -1,19 +1,8 @@
 import { Sprite } from "./sprite.js";
-import { IMAGES } from "../images/index.js";
-
-const bonuses = [
-  IMAGES.APPLE_RED,
-  IMAGES.APPLE_GREEN,
-  IMAGES.WATERMELON,
-  IMAGES.BANANA,
-  IMAGES.EGGPLANT
-];
+import { FOOD, getRandomKey } from "../images/index.js";
 
 export class Apple extends Sprite {
   constructor({ x, y }) {
-    const getRandomBonusImg = () =>
-      bonuses[Math.floor(Math.random() * bonuses.length)];
-
-    super({ x, y, image: getRandomBonusImg() });
+    super({ x, y, image: getRandomKey(FOOD) });
   }
 }
