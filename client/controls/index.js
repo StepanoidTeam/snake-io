1176d672ddca9909a414d6145f8809d0466c59aa
bobanko <mishko.vladimir@gmail.com>
2019-05-12 00:51:01 +0300

    client: touch controls fix + styling

--- client/controls/index.js
+++ client/controls/index.js
@@ -1,10 +1,12 @@
 import { initKeyboard } from "./keyboard.js";
 import { initTouch } from "./touch.js";
 
-const IS_TOUCH_DEVICE = navigator.MaxTouchPoints > 0;
+const IS_TOUCH_DEVICE = "ontouchstart" in window;
 
 export function setupControls() {
   let initControls = IS_TOUCH_DEVICE ? initTouch : initKeyboard;
+
+  if (IS_TOUCH_DEVICE) document.body.classList.add("is-touch");
 
   return initControls();
 }
